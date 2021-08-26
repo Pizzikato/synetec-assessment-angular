1474313b4bb165e7e256d90b296276b1b93bbe56
Vito <v.alfarano@gmail.com>
2021-08-26 18:38:07 +0200

    Update cities-list.component.ts

--- src/app/components/cities/cities-list.component.ts
+++ src/app/components/cities/cities-list.component.ts
@@ -1,5 +1,7 @@
+import { HttpErrorResponse } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { ICity } from "../../models/city.model";
+import { CitiesService } from "../../services/cities/cities.service";
 
 @Component ({
     selector: 'cities-list',
@@ -10,8 +12,34 @@ import { ICity } from "../../models/city.model";
 export class CitiesListComponent implements OnInit{
 
     cities: ICity[];
-    constructor() {}
+    message: string;
+    constructor(private _citiesService: CitiesService) {}
 
     ngOnInit(): void {
+
+        this._citiesService.getCities().subscribe(
+            (cities: ICity[]) => {
+              this.cities = cities;
+              if (!cities) this.message = " is Empty!"
+            },
+            (error: HttpErrorResponse) => {
+              this.message =  'Error reading cities';
+            }
+          );
+    }
+
+
+    delete(id: number): void {
+
+      this._citiesService.deleteCity(id).subscribe(
+        () => {
+          this.message = "City deleted Successful";
+          this.cities = this.cities.filter(x => x.id !== id);
+        },
+        (error: HttpErrorResponse) => {
+          this.message =  'Error deleting city';
+        }
+      );
+
     }
 }
