9f06ad7f30015f72662f6281971b5b5e4b1ede09
Vito <v.alfarano@gmail.com>
2021-08-28 08:27:02 +0200

    Update cities-endpoint.service.ts

--- src/app/services/cities/cities-endpoint.service.ts
+++ src/app/services/cities/cities-endpoint.service.ts
@@ -3,19 +3,22 @@ import { environment } from "../../../environments/environment";
 import { BaseService } from "../base.service";
 import { Observable } from "rxjs/Observable";
 import { HttpClient } from "@angular/common/http";
+import { ICity } from "../../models/city.model";
 
 @Injectable()
 export class CitiesEndpoint extends BaseService {
 
+    private url = `${this.getBaseUrl()}api/cities`;
+    
     constructor(private _httpClient: HttpClient, private _injector: Injector) {
         super(_httpClient, _injector);
     }
-public getCities():Observable<ICity[]> {
-return this._httpClient.get().subscribe();
-} 
+    public getCities(): Observable<ICity[]> {
+        return this._httpClient.get<ICity[]>(this.url, this.getRequestHeaders());
+    }
 
-public deleteCity(id:number):Observable<ICity>{
-return this._httpClient.delete().subscribe();
-} 
+    public deleteCity(id: number): Observable<void> {
+        return this._httpClient.delete<void>(`${this.url}/delete-city/${id}`, this.getRequestHeaders());
+    }
 
 }
