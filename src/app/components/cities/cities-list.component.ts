ec0681c157b03de2cbd1893be05564a2ae6969d3
Vito <v.alfarano@gmail.com>
2021-08-28 08:26:11 +0200

    Update cities-list.component.ts

--- src/app/components/cities/cities-list.component.ts
+++ src/app/components/cities/cities-list.component.ts
@@ -1,45 +1,49 @@
 import { HttpErrorResponse } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "../../../environments/environment";
 import { ICity } from "../../models/city.model";
 import { CitiesService } from "../../services/cities/cities.service";
 
-@Component ({
-    selector: 'cities-list',
-    templateUrl: './cities-list.component.html',
-    styleUrls: ['./cities-list.component.css']
+@Component({
+  selector: 'cities-list',
+  templateUrl: './cities-list.component.html',
+  styleUrls: ['./cities-list.component.css']
 })
 
-export class CitiesListComponent implements OnInit{
+export class CitiesListComponent implements OnInit {
 
-    cities: ICity[];
-    message: string;
-    constructor(private _citiesService: CitiesService) {}
+  cities: ICity[];
+  message: string;
+  constructor(private _citiesService: CitiesService) { }
 
-    ngOnInit(): void {
+  ngOnInit(): void {
+    this.getCities();
+  }
 
-        this._citiesService.getCities().subscribe(
-            (cities: ICity[]) => {
-              this.cities = cities;
-              if (!cities) this.message = " is Empty!"
-            },
-            (error: HttpErrorResponse) => {
-              this.message =  'Error reading cities';
-            }
-          );
-    }
+  getCities() {
+    this._citiesService.getCities().subscribe(
+      (cities: ICity[]) => {
+        this.cities = cities;
+      },
+      (error: HttpErrorResponse) => {
+        this.message = 'Error loading cities';
+      }
+    );
+  }
 
+  delete(city: ICity) {
 
-    delete(id: number): void {
-
-      this._citiesService.deleteCity(id).subscribe(
-        () => {
-          this.message = "City deleted Successful";
-          this.cities = this.cities.filter(x => x.id !== id);
-        },
-        (error: HttpErrorResponse) => {
-          this.message =  'Error deleting city';
+    if (confirm(`Are you sure you wish to delete ${city.name} city ?`)) {
+      this._citiesService.deleteCity(city.id).subscribe(() => {
+        if (environment.production) this.getCities();
+        else {
+          const index = this.cities.findIndex(c => c.id === city.id);
+          this.cities.splice(index, 1);
         }
-      );
-
+        this.message = "City deleted Successful";
+      });
     }
+
+  }
 }
